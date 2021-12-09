ad35afff592c64e2e3e95d137b06ca2fd8711a8e
Julien Cassis <juliencassis@gmail.com>
2021-12-09 15:23:22 -0500

    added transform registry

--- transform/builder.go
+++ transform/builder.go
@@ -1,18 +1,22 @@
 package transform
 
+import (
+	"github.com/streamingfast/bstream"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
 // Une fonction qui lis la request.BlockStreamV2, qui lookup le registry et retourne une erreur si yé pas là.. et build up une liste de `myTransforms`  pour passer ici-bas
 
 // Une fonction qui prends la liste de mes transforms, pré-vetted, et qui build une PREPROC FUNC
 
-//func BuildFrom(listOfTransforms []anypb.Any) bstream.ProcessingFunc {
-//	return func(blk *Block) (interface{}, error) {
-//		var in Input
-//		var output Output
-//		for transform := range myTransforms {
-//			output = transform.Transform(prevVal)
-//			prevVal = toInput(output)
-//
-//		}
-//		return output, nil
-//	}
-//}
+func BuildFromTransforms(transforms []anypb.Any) bstream.PreprocessFunc {
+	return func(blk *bstream.Block) (interface{}, error) {
+		var in Input
+		_ = in
+		var output Output
+		for _, transform := range transforms {
+			_ = transform
+		}
+		return output, nil
+	}
+}
--- transform/registry.go
+++ transform/registry.go
@@ -1,3 +1,34 @@
 package transform
 
-var Registry map[string]Transform
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type NewTransformFunc func(p proto.Message) (Transform, error)
+
+var registry = make(map[protoreflect.FullName]NewTransformFunc)
+
+func Register(name protoreflect.FullName, f NewTransformFunc) {
+	if name == "" {
+		zlog.Fatal("name cannot be blank")
+	} else if _, ok := registry[name]; ok {
+		zlog.Fatal("already registered", zap.String("name", string(name)))
+	}
+	registry[name] = f
+}
+
+func New(message proto.Message) (Transform, error) {
+	key := proto.MessageName(message)
+	newFunc, found := registry[key]
+	if !found {
+		return nil, fmt.Errorf("no such transform registered %q", key)
+	}
+	transform, err := newFunc(message)
+	if err != nil {
+		return nil, fmt.Errorf("unable to setup transform: %w", err)
+	}
+	return transform, nil
+}
