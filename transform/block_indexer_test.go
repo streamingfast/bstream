7bc1122de6320a649e6baa2c3c055f772fe821ce
Francis Roch <francis@manafont.net>
2022-02-22 12:54:02 -0500

    Flesh out TestBlockIndexer_ReadIndex()

--- transform/block_indexer_test.go
+++ transform/block_indexer_test.go
@@ -1,10 +1,8 @@
 package transform
 
 import (
-	"github.com/streamingfast/bstream"
 	"github.com/streamingfast/dstore"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
 	"io"
 	"io/ioutil"
 	"testing"
@@ -58,7 +56,7 @@ func TestBlockIndexer_WriteIndex(t *testing.T) {
 			},
 		},
 		{
-			name:              "expect error due to nil index; blockNum % indexSize != 0",
+			name:              "expect error due to nil index; lowBlockNum % indexSize != 0",
 			indexSize:         3,
 			indexShortname:    "test",
 			expectError:       true,
@@ -83,7 +81,7 @@ func TestBlockIndexer_WriteIndex(t *testing.T) {
 			results := make(map[string][]byte)
 
 			// spawn a dstore
-			accountIndexStore := dstore.NewMockStore(func(base string, f io.Reader) error {
+			indexStore := dstore.NewMockStore(func(base string, f io.Reader) error {
 				content, err := ioutil.ReadAll(f)
 				require.NoError(t, err)
 				results[base] = content
@@ -91,7 +89,7 @@ func TestBlockIndexer_WriteIndex(t *testing.T) {
 			})
 
 			// spawn an indexer and feed it
-			indexer := NewBlockIndexer(accountIndexStore, test.indexSize, "test")
+			indexer := NewBlockIndexer(indexStore, test.indexSize, "test")
 			for blockNum, keys := range test.kv {
 				indexer.Add(keys, blockNum)
 			}
@@ -103,7 +101,6 @@ func TestBlockIndexer_WriteIndex(t *testing.T) {
 			} else {
 				require.NoError(t, err)
 				// check the contents
-				zlog.Info("write", zap.Any("results", results))
 				require.Equal(t, test.expectedResultLen, len(results))
 				for k, _ := range results {
 					require.Equal(t, k, toIndexFilename(test.indexSize, 10, test.indexShortname))
@@ -115,22 +112,81 @@ func TestBlockIndexer_WriteIndex(t *testing.T) {
 
 func TestBlockIndexer_ReadIndex(t *testing.T) {
 	tests := []struct {
-		name            string
-		indexSize       uint64
-		shouldWriteFile bool
-		blocks          []*bstream.Block
+		name                string
+		indexSize           uint64
+		indexShortname      string
+		kv                  map[uint64][]string
+		expectedKvAfterRead map[string][]uint64
 	}{
 		{
-			name:            "sunny within bounds",
-			indexSize:       10,
-			shouldWriteFile: false,
-			blocks:          []*bstream.Block{},
+			name:           "sunny path",
+			indexSize:      10,
+			indexShortname: "test",
+			kv: map[uint64][]string{
+				10: {
+					"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+					"cccccccccccccccccccccccccccccccccccccccc",
+				},
+				11: {
+					"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+				},
+			},
+			expectedKvAfterRead: map[string][]uint64{
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa": {10, 11},
+				"bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb": {10, 11},
+				"cccccccccccccccccccccccccccccccccccccccc": {10},
+			},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			results := make(map[string][]byte)
 
+			// spawn a dstore
+			indexStore := dstore.NewMockStore(func(base string, f io.Reader) error {
+				content, err := ioutil.ReadAll(f)
+				require.NoError(t, err)
+				results[base] = content
+				return nil
+			})
+
+			// spawn an indexer and feed it
+			indexer := NewBlockIndexer(indexStore, test.indexSize, "test")
+			for blockNum, keys := range test.kv {
+				indexer.Add(keys, blockNum)
+			}
+
+			// write the index to dstore
+			err := indexer.WriteIndex()
+			require.NoError(t, err)
+
+			// populate a new indexStore with the prior results
+			indexStore = dstore.NewMockStore(nil)
+			for indexName, indexContents := range results {
+				indexStore.SetFile(indexName, indexContents)
+			}
+
+			// spawn a new BlockIndexer with the new IndexStore
+			indexer = NewBlockIndexer(indexStore, test.indexSize, test.indexShortname)
+
+			for indexName, _ := range results {
+				// attempt to read back the index
+				err = indexer.ReadIndex(indexName)
+				require.NoError(t, err)
+
+				// check our resulting KV
+				require.NotNil(t, indexer.KV())
+				require.Equal(t, len(indexer.KV()), len(test.expectedKvAfterRead))
+				for expectedK, expectedV := range test.expectedKvAfterRead {
+					actualV, ok := indexer.KV()[expectedK]
+					require.True(t, ok)
+					arr := actualV.ToArray()
+					require.Equal(t, arr, expectedV)
+				}
+			}
 		})
 	}
 }
