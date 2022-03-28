e36a9f64f9583de518a716266954cc071a1c5cc2
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-03-28 15:40:52 -0400

    registry can return PassthroughTransform (substreams)

--- transform/builder.go
+++ transform/builder.go
@@ -1,24 +1,46 @@
 package transform
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
 	"github.com/streamingfast/bstream"
+	pbfirehose "github.com/streamingfast/pbgo/sf/firehose/v1"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 )
 
+type PassthroughTransform interface {
+	String() string
+	Run(ctx context.Context, req *pbfirehose.Request, output func(*bstream.Cursor, *anypb.Any) error)
+}
+
 // BuildFromTransforms returns a PreprocessFunc, an optional BlockIndexProvider, a human-readable description and an error
-func (r *Registry) BuildFromTransforms(anyTransforms []*anypb.Any) (bstream.PreprocessFunc, bstream.BlockIndexProvider, string, error) {
+func (r *Registry) BuildFromTransforms(anyTransforms []*anypb.Any) (
+	bstream.PreprocessFunc,
+	bstream.BlockIndexProvider,
+	PassthroughTransform,
+	string,
+	error,
+) {
 	var blockIndexProvider bstream.BlockIndexProvider
 	transforms := []Transform{}
 	for _, transform := range anyTransforms {
 		t, err := r.New(transform)
 		if err != nil {
-			return nil, nil, "", fmt.Errorf("unable to instantiate transform: %w", err)
+			return nil, nil, nil, "", fmt.Errorf("unable to instantiate transform: %w", err)
 		}
 		transforms = append(transforms, t)
+
+		// passthrough runner currently highjacks all the other transforms
+		if pr, ok := t.(PassthroughTransform); ok {
+			if len(anyTransforms) != 1 {
+				return nil, nil, nil, "", fmt.Errorf("transform %s is incompatible with any other transforms and must be used alone", pr.String())
+			}
+			return nil, nil, pr, pr.String(), nil
+		}
+
 		if bipg, ok := t.(bstream.BlockIndexProviderGetter); ok {
 			if blockIndexProvider != nil { // TODO eventually, should we support multiple indexes ?
 				zlog.Warn("multiple index providers from transform, ignoring")
@@ -56,5 +78,5 @@ func (r *Registry) BuildFromTransforms(anyTransforms []*anypb.Any) (bstream.Prep
 		}
 		return out, nil
 	}
-	return preprocessFunc, blockIndexProvider, descriptions, nil
+	return preprocessFunc, blockIndexProvider, nil, descriptions, nil
 }
