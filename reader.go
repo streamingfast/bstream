a98153ba4b86e0326c4632ff15a1e648d9b8b698
Charles Billette <charles@cbillette.com>
2024-02-07 10:45:57 -0500

    Feature/sol poller (#35)
    * added ACCEPT_SOLANA_LEGACY_BLOCK_FORMAT env var check to allow read legacy block

--- reader.go
+++ reader.go
@@ -17,6 +17,7 @@ package bstream
 import (
 	"fmt"
 	"io"
+	"os"
 
 	pbbstream "github.com/streamingfast/bstream/pb/sf/bstream/v1"
 
@@ -116,6 +117,10 @@ func supportLegacy(b *pbbstream.Block) error {
 		case pbbstream.Protocol_COSMOS:
 			b.Payload.TypeUrl = "type.googleapis.com/sf.cosmos.type.v1.Block"
 		case pbbstream.Protocol_SOLANA:
+			if _, ok := os.LookupEnv("ACCEPT_SOLANA_LEGACY_BLOCK_FORMAT"); ok {
+				b.Payload.TypeUrl = "type.googleapis.com/sf.solana.type.v1.Block"
+				break
+			}
 			return fmt.Errorf("old block format from Solana protocol not supported, migrate your blocks")
 		case pbbstream.Protocol_NEAR:
 			return fmt.Errorf("old block format from NEAR protocol not supported, migrate your blocks")
